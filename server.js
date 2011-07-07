94afbe5624f8f80960bf71bd63f4a9266fe13c36
T.J. Purtell <tpurtell@stanford.edu>
2011-07-07 03:23:56 -0700

    demo version

---
+++ server.js
@@ -0,0 +1,51 @@
+// embed.png source http://en.wikipedia.org/wiki/File:Holliday_Junction.png
+var PORT = 8888;
+var REDIRECT = "http://openjunction.org/";
+var SERVER = "http://wrldsuksgo2mars.doesntexist.org:8888"
+String.prototype.startsWith = function(prefix){
+    return this.lastIndexOf(prefix, 0) === 0;
+}
+String.prototype.endsWith = function(suffix){
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+}
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var ejs = require('../ejs');
+
+console.log('Loading embed template...');
+var embed_template = ejs.compile(fs.readFileSync('embed.ejs', 'utf8'));
+
+console.log('Loading embed image...');
+var embed_image = fs.readFileSync('embed.png');
+
+console.log('Loading plugin...');
+var embed_crx = fs.readFileSync('g++.crx');
+
+http.createServer(function (req, res) {
+    if(req.url.startsWith('/embed')) {
+        var query = url.parse(req.url, true).query;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(
+            embed_template({
+                app:encodeURIComponent(query.app), 
+                server:SERVER
+            }));
+    } else if(req.url.startsWith('/image')) {
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end(embed_image);
+    } else if(req.url.startsWith('/extension')) {
+        res.writeHead(200, {'Content-Type': 'application/x-chrome-extension'});
+        res.end(embed_crx);
+    } else if(req.url.length <= 1){
+        res.writeHead(302, {'Location': REDIRECT});
+        res.end();
+    } else {
+        res.writeHead(404);
+        res.end("Not found!");
+    }
+}).listen(PORT);
+
+console.log('Server running at http://*:' + PORT + '/');
